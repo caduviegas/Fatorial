6c1ebca45f2f4bff7947c1aeda8231f6600526fc
Carlos Eduardo Viegas <eduardoviegas@brq.com>
2022-09-11 08:45:56 -0300

    Criação da regra de negocio e xml

--- app/src/main/java/com/innaval/fatorial/MainActivity.java
+++ app/src/main/java/com/innaval/fatorial/MainActivity.java
@@ -2,13 +2,47 @@ package com.innaval.fatorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.math.BigInteger;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    EditText editText;
+    TextView textView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.et_calculofatorial);
+        textView = findViewById(R.id.tv_resposta);
+        button = findViewById(R.id.btn_calcular);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int num = Integer.parseInt(editText.getText().toString());
+                textView.setText("" + String.format(Locale.getDefault(), "%.2f", factorial(num)));
+            }
+        });
+    }
+
+    private double factorial(double num) {
+        if (num < 2) {
+            return 1;
+        } else {
+            return num * factorial(num - 1);
+        }
     }
 }
+
